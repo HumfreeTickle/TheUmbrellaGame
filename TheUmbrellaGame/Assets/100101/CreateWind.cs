bdf5d3f73d4779f7a669413f434821e021b3756f
Stephen Larkin <stephenlarkin64@gmail.com>
2015-07-09 11:26:03 +0100

    Wind Stuff

--- TheUmbrellaGame/Assets/100101/CreateWind.cs
+++ TheUmbrellaGame/Assets/100101/CreateWind.cs
@@ -7,16 +7,17 @@ public class CreateWind : MonoBehaviour
 	public GameObject windSystem;
 	private GameObject instatiatedWind;
 	private Vector3 spawnDistance;
-	public float charge = 0;
+	public float charge = 100;
 	private float _timer;
 
 	void Update ()
 	{
-		if (transform.position.y <= 30 && transform.childCount == 0) {
-			windFalling ();
-		}
-		if (Input.GetButtonDown ("Fire1") && transform.childCount == 0) {
+//		if (transform.position.y <= 30 && transform.childCount == 0) {
+//			windFalling ();
+//		}
+		if (Input.GetButtonDown ("Fire1") && charge >= 1) {
 			SummonWind ();
+			charge -=  10; //Mathf.Lerp (charge, 0, Time.time/10);
 		}
 //		Charging ();
 		Mathf.Clamp (_timer, 0, 20);
@@ -33,13 +34,12 @@ public class CreateWind : MonoBehaviour
 
 	void SummonWind ()
 	{
-		print ("wind");
 		spawnDistance = transform.position - new Vector3 (0, 10, 0);
 		
 		instatiatedWind = Instantiate (windSystem, spawnDistance, Quaternion.Euler (Vector3.forward)) as GameObject;
 		instatiatedWind.transform.parent = this.transform; 
 		instatiatedWind.GetComponent<ParticleSystem> ().enableEmission = true;
-		instatiatedWind.GetComponent<wind> ().windForce = charge;
+		instatiatedWind.GetComponent<wind> ().windForce = charge * 10;
 
 		if (Input.GetButtonUp ("Fire1")) {
 			instatiatedWind.GetComponent<ParticleSystem> ().enableEmission = false;
--- TheUmbrellaGame/Assets/rayCastToFuck.cs
+++ TheUmbrellaGame/Assets/rayCastToFuck.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class rayCastToFuck : MonoBehaviour
 {
-	public GameObject hitPoint;
 	public float chargeDistance;
+	private Material umbrellaColour;
+	private Vector3 baseUmbrella = new Vector3 (0f, -2f, 0f);
+
+	void Start ()
+	{
+		umbrellaColour = GetComponent<Renderer> ().material;
+		chargeDistance = GameObject.Find ("main_Sphere").GetComponent<CreateWind> ().charge; 
+		chargeDistance = 100;
+	}
 
 	void Update ()
 	{
+		umbrellaColour.SetColor ("_Color", new Vector4 (chargeDistance / 100, chargeDistance / 100, chargeDistance / 100, 1));
+		chargeDistance = GameObject.Find ("main_Sphere").GetComponent<CreateWind> ().charge; 
 		Vector3 downRayDown = Vector3.down;
 		RaycastHit hit;
 		
-		if (Physics.Raycast (transform.position, downRayDown, out hit, Mathf.Clamp(chargeDistance, 1, Mathf.Infinity))) {
-			if(hit.collider.tag == "Terrain"){
-				Instantiate(hitPoint, hit.point, Quaternion.identity);
-				GameObject.Find("main_Sphere") .GetComponent<CreateWind>().charge += chargeDistance;
+		if (Physics.Raycast (transform.position + baseUmbrella , downRayDown, out hit, Mathf.Infinity)) {
+			Debug.DrawRay(transform.position, downRayDown, Color.green, Mathf.Infinity, false);
+			if (hit.collider.tag == "Terrain") {
+				print (hit.distance);
+				chargeDistance = Mathf.Lerp (chargeDistance, 100, Time.time/(hit.distance*100));
+				GameObject.Find ("main_Sphere").GetComponent<CreateWind> ().charge = chargeDistance;
 			}
-		}
-		
-
+		}	
 	}
 }
