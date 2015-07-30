b2474b3f85691a408ffd989bd32241d6ac20ceb1
Stephen Larkin <stephenlarkin64@gmail.com>
2015-07-30 12:28:21 +0100

    Lovely pretty little changes

---
+++ TheUmbrellaGame/Assets/100101/_NPC/TheFollower.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NPC
+{
+	public class TheFollower : MonoBehaviour
+	{
+
+		Transform THEtarget;
+		public float maxRange;
+		public float minRange;
+		public bool inSight;
+		public float speed;
+		public GameObject dropOFF;
+		private Animator animator;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			inSight = false;
+			THEtarget = GameObject.FindGameObjectWithTag ("Player").transform;
+			animator = GetComponent<Animator> ();
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+			if ((Vector3.Distance (transform.position, THEtarget.position) < maxRange)
+				&& (Vector3.Distance (transform.position, THEtarget.position) > minRange)) {
+
+				Debug.Log ("ShouldSee");
+				inSight = true;
+
+			}
+			if (Vector3.Distance (transform.position, THEtarget.position) > maxRange) {
+
+				Debug.Log ("ShouldStop");
+				inSight = false;
+
+
+			}
+
+			if (inSight) {
+
+				Debug.Log ("TRUE");
+				transform.LookAt (THEtarget);
+				transform.Translate (Vector3.forward * Time.deltaTime * speed);
+				dropOFF.SetActive (true);
+				animator.SetBool ("WillWalk", true);
+		
+
+			}
+			if (!inSight) {
+
+				Debug.Log ("FALSE");
+				dropOFF.SetActive (false);
+				animator.SetBool ("WillWalk", false);
+			}
+	
+		}
+
+		void OnTriggerEnter (Collider other)
+		{
+
+			if (other.gameObject.tag == "Dropper") {
+
+				Destroy (gameObject);
+				Destroy (dropOFF);
+			}
+		}
+	}
+}
+
